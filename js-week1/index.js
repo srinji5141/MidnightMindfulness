87868ad8554b7f4d1e66b3e3df4272d4bfa1c1a6
srinji5141 <105142919+srinji5141@users.noreply.github.com>
2023-09-01 00:58:05 -0700

    Delete js-week1 directory

--- js-week1/index.js
+++
@@ -1,8 +0,0 @@
-let intention = [" Take it easy 😎 ", " Be present and connected 🎁 ", 
-" Set a goal for today 🦾 ", " Forgive others and yourself 🫀 ", 
-" Que sera, sera ✨ ", " Today's a new day 🌅 ", 
-" One step at a time 🧗‍♀️ "];
-function fetchIntention(){
-    let randNo = Math.floor(Math.random()*intention.length);
-    document.getElementById("intention").innerHTML = intention[randNo];
-}
