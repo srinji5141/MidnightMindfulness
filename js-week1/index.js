14b16ad0db1d51d491e883c9a2ece37c4413884e
srinji5141 <105142919+srinji5141@users.noreply.github.com>
2023-09-01 00:31:38 -0700

    Update index.js

--- js-week1/index.js
+++ js-week1/index.js
@@ -1,8 +1,7 @@
-let intention=[" Take it easy 😎 ", " Be present and connected 🎁 ", 
-" Think of 1 goal you want to achieve and achieve it today 🦾 ", " Forgive others and yourself 🫀 ", 
-" Que sera, sera ✨ ", " Carpe diem 🎢 ", 
-" Today's a new day 🌅 ", " Memento mori 💀 ", 
-" One step at a time 🧗‍♀️ ", " Enjoy it as it goes 🕉 "];
+let intention = [" Take it easy 😎 ", " Be present and connected 🎁 ", 
+" Set a goal for today 🦾 ", " Forgive others and yourself 🫀 ", 
+" Que sera, sera ✨ ", " Today's a new day 🌅 ", 
+" One step at a time 🧗‍♀️ "];
 function fetchIntention(){
     let randNo = Math.floor(Math.random()*intention.length);
     document.getElementById("intention").innerHTML = intention[randNo];
